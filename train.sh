9b3ee22fbc95f73a3d1eb63758e6950ea4eb14e8
Clarivy <zqc.sc.hn@gmail.com>
2022-04-26 05:08:20 +0000

    Add: tensorboard support

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --gpu_ids 3 --no_instance --dataroot /data/new_disk/pangbai/d2sn_data/train --label_nc 0 --name no_agu
+python train.py --gpu_ids 1 --no_instance --dataroot /data/new_disk/pangbai/d2sn_data/train --label_nc 0 --name d2sn --tf_log
--- util/visualizer.py
+++ util/visualizer.py
@@ -18,10 +18,9 @@ class Visualizer():
         self.win_size = opt.display_winsize
         self.name = opt.name
         if self.tf_log:
-            import tensorflow as tf
-            self.tf = tf
-            self.log_dir = os.path.join(opt.checkpoints_dir, opt.name, 'logs')
-            self.writer = tf.summary.FileWriter(self.log_dir)
+            from torch.utils.tensorboard import SummaryWriter
+            self.log_dir = os.path.join(opt.checkpoints_dir, 'logs', opt.name)
+            self.writer = SummaryWriter(self.log_dir)
 
         if self.use_html:
             self.web_dir = os.path.join(opt.checkpoints_dir, opt.name, 'web')
@@ -39,19 +38,14 @@ class Visualizer():
             img_summaries = []
             for label, image_numpy in visuals.items():
                 # Write the image to a string
-                try:
-                    s = StringIO()
-                except:
-                    s = BytesIO()
-                scipy.misc.toimage(image_numpy).save(s, format="jpeg")
-                # Create an Image object
-                img_sum = self.tf.Summary.Image(encoded_image_string=s.getvalue(), height=image_numpy.shape[0], width=image_numpy.shape[1])
-                # Create a Summary value
-                img_summaries.append(self.tf.Summary.Value(tag=label, image=img_sum))
+                # try:
+                #     s = StringIO()
+                # except:
+                #     s = BytesIO()
+                # scipy.misc.toimage(image_numpy).save(s, format="jpeg")
+                # Write image to tensorboard
+                self.writer.add_image(label, image_numpy, step, dataformats = 'HWC')
 
-            # Create and write Summary
-            summary = self.tf.Summary(value=img_summaries)
-            self.writer.add_summary(summary, step)
 
         if self.use_html: # save images to a html file
             for label, image_numpy in visuals.items():
@@ -95,8 +89,9 @@ class Visualizer():
     def plot_current_errors(self, errors, step):
         if self.tf_log:
             for tag, value in errors.items():
-                summary = self.tf.Summary(value=[self.tf.Summary.Value(tag=tag, simple_value=value)])
-                self.writer.add_summary(summary, step)
+                # summary = self.tf.Summary(value=[self.tf.Summary.Value(tag=tag, simple_value=value)])
+                # self.writer.add_summary(summary, step)
+                self.writer.add_scalar(tag, value, step)
 
     # errors: same format as |errors| of plotCurrentErrors
     def print_current_errors(self, epoch, i, errors, t):
